dc5627bb7f0335713f45d1dd59eee028e6258bc0
Drake Fafard <69027132+DarkDonut0207@users.noreply.github.com>
2023-05-03 19:59:06 -0700

    Added scripts folder

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController characterController;
+
+    public float speed;
+    public float rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Imported
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        float magnitude = Mathf.Clamp01(movementDirection.magnitude) * speed;
+        movementDirection.Normalize();
+
+        characterController.SimpleMove(movementDirection * magnitude);
+
+        if(movementDirection != Vector3.zero) {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
